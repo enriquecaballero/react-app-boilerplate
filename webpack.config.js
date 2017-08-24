97661387fe36981ba9142d50021369774c887bbb
Enrique Caballero <ecaballerojr@icloud.com>
2017-08-24 16:44:25 -0600

    chore: Ran prettier on webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = () => {
   switch (process.env.npm_lifecycle_event) {
     case "start":
-      return require("./config/webpack.config.server");
+      return require ("./config/webpack.config.server");
     default:
-      return require("./config/webpack.config.build");
+      return require ("./config/webpack.config.build");
   }
 };
